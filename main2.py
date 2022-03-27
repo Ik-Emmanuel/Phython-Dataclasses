ae06b78a45161a5889ff7950df228eeca62c4ee6
Ikechukwu Emmanuel <nwokochaikechukwu13@gmail.com>
2022-03-27 21:06:13 +0100

    update: added a new example main2.py

---
+++ main2.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+
+@dataclass(order=True) #allows you compare different class instance
+class Person:
+    sort_index: int = field(init=False, repr=False) #allow you sort by a given attribute 
+    name: str
+    job: str
+    age: int
+    strength: int = 100
+
+    def __post_init__(self):
+        self.sort_index = self.age
+
+person1 = Person("Kelly", "Clarkson", 20)
+person2 = Person("Joe", "Regan", 30)
+person3 = Person("Melly", "Buns", 40)
+
+print(person2)
+print(person3)
+
+print(person2 > person3)
